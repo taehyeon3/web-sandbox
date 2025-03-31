10163a19eadea1da0d26d5480929355ba202a9a0
taehyeon <kth0373@naver.com>
2025-03-31 18:30:25 +0900

    feat: swagger 추가

---
+++ backend-board/src/main/java/com/backendboard/global/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.backendboard.global.config;
+
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		SecurityScheme securityScheme = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER)
+			.name("Authorization");
+
+		return new OpenAPI()
+			.components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+			.info(new Info()
+				.title("샌드박스 API 문서")
+				.description("샌드박스 API 명세서")
+				.version("1.0.0"));
+	}
+}
