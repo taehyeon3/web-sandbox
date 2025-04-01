b5b7976899bb403519aaa518d3606b2b57d110b7
taehyeon <kth0373@naver.com>
2025-04-01 22:54:33 +0900

    refactor: ErrorResponse 레코드 클래스 어노테이션 제거

--- backend-board/src/main/java/com/backendboard/global/error/dto/ErrorResponse.java
+++ backend-board/src/main/java/com/backendboard/global/error/dto/ErrorResponse.java
@@ -5,10 +5,8 @@ import org.springframework.http.ResponseEntity;
 import com.backendboard.global.error.CustomException;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
 
 @Schema(description = "에러 응답 DTO")
-@Getter
 public record ErrorResponse(@Schema(description = "에러 메시지", example = "에러가 발생했습니다.") String message) {
 	public static ResponseEntity<ErrorResponse> toResponseEntity(final CustomException exception) {
 		return ResponseEntity.status(exception.getError().getStatus())
