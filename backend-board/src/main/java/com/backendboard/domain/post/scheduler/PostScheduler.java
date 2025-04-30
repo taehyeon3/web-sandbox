21933024c674a61164bbcb3d22696656d4946d3d
taehyeon3 <117972735+taehyeon3@users.noreply.github.com>
2025-04-30 16:50:01 +0900

    ⚡️ [Improve] 좋아요 기능 성능 개선 및 db 동기화 (#37)
    * feat: redis hashtable 관련 설정 추가
    
    * feat: 게시글 좋아요를 관리하는 redisRepository 추가
    
    * feat: 좋아요 수 동기화시키는 스케줄러 추가
    
    ---------
    
    Co-authored-by: taehyeon <kth0373@naver.com>

---
+++ backend-board/src/main/java/com/backendboard/domain/post/scheduler/PostScheduler.java
@@ -0,0 +1,39 @@
+package com.backendboard.domain.post.scheduler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.backendboard.domain.postlike.repository.PostLikeRedisRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PostScheduler {
+	private static final String UPDATE_LIKE_COUNT_SQL = "UPDATE post SET like_count = ? WHERE id = ?";
+
+	private final PostLikeRedisRepository postLikeRedisRepository;
+	private final JdbcTemplate jdbcTemplate;
+
+	@Scheduled(fixedDelay = 300_000)
+	public void syncLikeCount() {
+		Map<Object, Object> entries = postLikeRedisRepository.getEntries();
+		List<Object[]> batchArgs = new ArrayList<>();
+
+		for (Map.Entry<Object, Object> entry : entries.entrySet()) {
+			String postId = (String)entry.getKey();
+			long likeCount = ((Number)entry.getValue()).longValue();
+			batchArgs.add(new Object[] {likeCount, Long.valueOf(postId)});
+		}
+
+		jdbcTemplate.batchUpdate(UPDATE_LIKE_COUNT_SQL, batchArgs);
+		postLikeRedisRepository.delete();
+	}
+}
---
+++ backend-board/src/main/java/com/backendboard/domain/postlike/repository/PostLikeRedisRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.backendboard.domain.postlike.repository;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class PostLikeRedisRepositoryImpl implements PostLikeRedisRepository {
+	private static final String KEY = "post:like";
+
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	@Override
+	public Long getCount(String postId) {
+		return Optional.ofNullable(redisTemplate.opsForHash().get(KEY, postId))
+			.map(value -> ((Number)value).longValue())
+			.orElse(null);
+	}
+
+	@Override
+	public void save(String postId, Long likeCount) {
+		redisTemplate.opsForHash().put(KEY, postId, likeCount);
+	}
+
+	@Override
+	public void delete() {
+		redisTemplate.delete(KEY);
+	}
+
+	@Override
+	public Map<Object, Object> getEntries() {
+		return redisTemplate.opsForHash().entries(KEY);
+	}
+}
