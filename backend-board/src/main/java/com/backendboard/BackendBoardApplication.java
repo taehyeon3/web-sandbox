c851aee5c0ef9fd93463f81837b327c67914b00c
taehyeon3 <117972735+taehyeon3@users.noreply.github.com>
2025-05-23 13:39:24 +0900

    ✨ [Feature] Eureka 서버 추가 (#49)
    * chore: .gitignore 추가
    
    * feat: EurekaServer 추가
    
    * feat: EurekaClient 추가

--- backend-board/src/main/java/com/backendboard/BackendBoardApplication.java
+++ backend-board/src/main/java/com/backendboard/BackendBoardApplication.java
@@ -2,9 +2,11 @@ package com.backendboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableDiscoveryClient
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
