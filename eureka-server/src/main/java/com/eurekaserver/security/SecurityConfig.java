b019c1bf25e8f78dc5e9a17c3121508643103235
taehyeon3 <kth0373@naver.com>
2025-05-23 11:21:14 +0900

    feat: EurekaServer 추가

---
+++ eureka-server/src/main/java/com/eurekaserver/security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.eurekaserver.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+			.httpBasic(Customizer.withDefaults())
+			.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+
+		UserDetails user = User.builder()
+			.username("admin")
+			.password(passwordEncoder().encode("1234"))
+			.roles("ADMIN")
+			.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
