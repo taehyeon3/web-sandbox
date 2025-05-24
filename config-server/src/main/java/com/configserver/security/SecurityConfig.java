3c231d2b2523628fa3fe6548120bb751ccdfe344
taehyeon3 <kth0373@naver.com>
2025-05-24 12:32:32 +0900

    refactor: SecurityConfig 수정

--- config-server/src/main/java/com/configserver/security/SecurityConfig.java
+++ config-server/src/main/java/com/configserver/security/SecurityConfig.java
@@ -6,21 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,15 +19,5 @@ public class SecurityConfig {
 			.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
 			.httpBasic(Customizer.withDefaults())
 			.build();
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails userDetails = User.builder()
-			.username("admin")
-			.password(passwordEncoder().encode("1234"))
-			.roles("ADMIN")
-			.build();
-		return new InMemoryUserDetailsManager(userDetails);
 	}
 }
